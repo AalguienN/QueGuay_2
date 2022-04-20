e0c54f8df799181cc38cb7e7692d6ff5e885a479
Jorge <jorgebaegar3@gmail.com>
2022-04-20 14:47:04 +0200

    signUp eliminado

--- POI_UPV/src/poiupv/FXMLInicioController.java
+++ POI_UPV/src/poiupv/FXMLInicioController.java
@@ -1,190 +1,41 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package poiupv;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-
 /**
+ * FXML Controller class
  *
- * @author svalero
+ * @author User
  */
 public class FXMLInicioController implements Initializable {
 
-
- 
-    //properties to control valid fieds values. 
-    private BooleanProperty validPassword;
-    private BooleanProperty validEmail;
-    private BooleanProperty equalPasswords;  
-
-    //private BooleanBinding validFields;
-    
-    //When to strings are equal, compareTo returns zero
-    private final int EQUALS = 0;  
     @FXML
     private TextField eemail;
     @FXML
     private Label lIncorrectEmail;
     @FXML
-    private PasswordField contraseña;
+    private Label lIncorrectEmail1;
     @FXML
-    private Label ContraseñaIncorrecta;
-    @FXML
-    private PasswordField contraseña2;
-    @FXML
-    private Label ContraseñaIncorrecta2;
-    @FXML
-    private Button Aceptar;
+    private Button Cancelar1;
     @FXML
     private Button Cancelar;
-    
-   
-    
 
     /**
-     * Updates the boolProp to false.Changes to red the background of the edit. 
-     * Makes the error label visible and sends the focus to the edit. 
-     * @param errorLabel label added to alert the user
-     * @param textField edit text added to allow user to introduce the value
-     * @param boolProp property which stores if the value is correct or not
+     * Initializes the controller class.
      */
-    private void manageError(Label errorLabel,TextField textField, BooleanProperty boolProp ){
-        boolProp.setValue(Boolean.FALSE);
-        showErrorMessage(errorLabel,textField);
-        textField.requestFocus();
- 
-    }
-    /**
-     * Updates the boolProp to true. Changes the background 
-     * of the edit to the default value. Makes the error label invisible. 
-     * @param errorLabel label added to alert the user
-     * @param textField edit text added to allow user to introduce the value
-     * @param boolProp property which stores if the value is correct or not
-     */
-    private void manageCorrect(Label errorLabel,TextField textField, BooleanProperty boolProp ){
-        boolProp.setValue(Boolean.TRUE);
-        hideErrorMessage(errorLabel,textField);
-        
-    }
-    /**
-     * Changes to red the background of the edit and
-     * makes the error label visible
-     * @param errorLabel
-     * @param textField 
-     */
-    private void showErrorMessage(Label errorLabel,TextField textField)
-    {
-        errorLabel.visibleProperty().set(true);
-        textField.styleProperty().setValue("-fx-background-color: #FCE5E0");    
-    }
-    /**
-     * Changes the background of the edit to the default value
-     * and makes the error label invisible.
-     * @param errorLabel
-     * @param textField 
-     */
-    private void hideErrorMessage(Label errorLabel,TextField textField)
-    {
-        errorLabel.visibleProperty().set(false);
-        textField.styleProperty().setValue("");
-    }
-
-
-    
-
-    
-    //=========================================================
-    // you must initialize here all related with the object 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-        validEmail = new SimpleBooleanProperty();
-        validPassword = new SimpleBooleanProperty();   
-        equalPasswords = new SimpleBooleanProperty();
-        
-        validPassword.setValue(Boolean.FALSE);
-        validEmail.setValue(Boolean.FALSE);
-        equalPasswords.setValue(Boolean.FALSE);
-        BooleanBinding validFields = Bindings.and(validEmail, validPassword)
-                 .and(equalPasswords);
-        
-        eemail.focusedProperty().addListener((observable,oldValue,newValue) -> {
-            if(!newValue){
-                checkEditEmail();
-            }
-            
-        });
-        contraseña.focusedProperty().addListener((observable,oldValue,newValue) -> {
-            if(!newValue){
-                checkEditPassword();
-            }
-        });
-        contraseña2.focusedProperty().addListener((observable,oldValue,newValue) -> {
-            if(!newValue){
-                checkEquals();
-            }
-        });
-        Aceptar.disableProperty().bind(Bindings.not(validFields));
-        Cancelar.setOnAction( (event)-> {
-                    Cancelar.getScene().getWindow().hide();
-        });
-    }
-    private void checkEditEmail(){
-        if(!Utils.checkEmail(eemail.textProperty().getValueSafe()))
-            manageError(lIncorrectEmail,eemail,validEmail);
-        else
-            manageCorrect(lIncorrectEmail,eemail,validEmail);
-    }
-    private void checkEditPassword(){
-        if(!Utils.checkPassword(contraseña.textProperty().getValueSafe()))
-            manageError(ContraseñaIncorrecta,contraseña,validPassword);
-        else
-            manageCorrect(ContraseñaIncorrecta,contraseña,validPassword);
-    }
-    private void checkEquals(){
-        if(contraseña.textProperty().getValueSafe().compareTo(
-            contraseña2.textProperty().getValueSafe()) != EQUALS){
-                showErrorMessage(ContraseñaIncorrecta2,contraseña2);
-                equalPasswords.setValue(Boolean.FALSE);
-                contraseña.textProperty().setValue("");
-                contraseña2.textProperty().setValue("");
-                contraseña.requestFocus();
-        }else
-            manageCorrect(ContraseñaIncorrecta2,contraseña2,equalPasswords);
-        
-    }
-
-    @FXML
-    private void AceptarActivo(ActionEvent event) {
-        eemail.textProperty().setValue("");
-        contraseña.textProperty().setValue("");
-        contraseña2.textProperty().setValue("");
-        
-        validEmail.setValue(Boolean.FALSE);
-        validPassword.setValue(Boolean.FALSE);
-        equalPasswords.setValue(Boolean.FALSE);
-    }
-         
-    public boolean isResizable(){
-        return false;
-    }
-
-        
-
-    }
+        // TODO
+    }    
+    
+}
