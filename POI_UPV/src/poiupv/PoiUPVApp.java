237f4470f271316b264807c1cadb79fc710f9765
Adrián Camacho <adricamachogarcia@gmail.com>
2022-04-20 17:45:39 +0200

    wey

--- POI_UPV/src/poiupv/PoiUPVApp.java
+++ POI_UPV/src/poiupv/PoiUPVApp.java
@@ -19,10 +19,10 @@ public class PoiUPVApp extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_Principal.fxml"));
         
         Scene scene = new Scene(root);
-        stage.setTitle("Puntos de interes UPV");
+        stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
